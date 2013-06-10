89661fbb42cd70a7c6401b69d8f16cfd615fa478
Derrick Lin <drrckln@gmail.com>
2013-06-10 00:07:09 -0700

    skeletal main, need more conditions for handling other types (generics? sets? maps? how to represent as strings)

--- problems/Main.java
+++ problems/Main.java
@@ -23,19 +23,45 @@ public class Main {
             for (Method m : classMethods) {
                 if (m.getName().equals(sMethodName)) {
                     Type[] types = m.getGenericParameterTypes();
+                    int length = input_args.length;
+                    Object[] obj_args = new Object[length];
+                    
+                    for (int i=0; i<length; i++) {
+                        Type type = types[i];
+                        String arg = input_args[i];
+                        
+                        //if (type.getClass().isAssignableFrom(arg.getClass())) {
+                            
+                        //}
+                        
+                        if (type.equals(int.class) || type.equals(Integer.class)) {
+                            obj_args[i] = new Integer(Integer.parseInt(arg));
+                        } else if (type.equals(String.class)) {
+                            obj_args[i] = arg;                            
+                        } else {
+                            obj_args[i] = arg; // do some transformation?
+                        }
+                    }
                     
                     
                     Object.class.isAssignableFrom(types[0].getClass());
-                    // how to dynamically cast and then invoke as params??
                             
                             
-                    String returnValue = (String) target_class.invoke(m, new Object[] { methodParameter });
+                    String returnValue = (String) m.invoke(null, obj_args);
+                    // If the underlying method is static, then the specified obj argument is ignored. It may be null.
+                    // If the number of formal parameters required by the underlying method is 0, the supplied args array may be of length 0 or null.
+                    // If the underlying method is static, the class that declared the method is initialized if it has not already been initialized.
+                    // If the method completes normally, the value it returns is returned to the caller of invoke; if the value has a primitive type, 
+                    // it is first appropriately wrapped in an object. However, if the value has the type of an array of a primitive type, the elements 
+                    // of the array are not wrapped in objects; in other words, an array of primitive type is returned. If the underlying method return 
+                    // type is void, the invocation returns null.
+
                     System.out.println(returnValue);
                 }
             }
             
         } catch (ClassNotFoundException e) {
-        } catch (NoSuchMethodException e) {
+        } catch (Exception e) {
         }
        
     }
