2f0b12b978a5b6ed20255cdaa3328d7c1f97b4e1
Derrick Lin <drrckln@gmail.com>
2013-06-25 15:57:53 -0700

    adding all

--- problems/tester.py
+++ problems/tester.py
@@ -1,34 +1,39 @@
-# use importlib
+# make classes for extra classes and then just parse them in
+# make version for proper programs
 
 import sys
 import subprocess
-# from py4j.java_collections import SetConverter, MapConverter, ListConverter
 
+# let's do just filename, first line is name of program
+# other lines will be input args?
 name = sys.argv[1]
 str_args = sys.argv[2:]
 argt = []
 for arg in str_args:
     argt.append(eval(arg))
 
-module = __import__(name)
 
-fx = getattr(module, name)
 
-# gateway = JavaGateway(auto_convert=True)
+def py_try(name,*args):
+    # import from within a folder?
+    module = __import__(name)
+    fx = getattr(module, name)
+    try:
+        return fx(*args)
+    except:
+        return sys.exc_info()
+
 
 
 def check(*args):
-    try:
-        py_out = fx(*args)
-    except:
-        py_out = sys.exc_info()
+    py_out = py_try(name,*args)
     p1 = subprocess.Popen(["/usr/bin/java", "Main", name]+str_args, stdout=subprocess.PIPE)
     java_out = p1.stdout.read()
-    if py_out == java_out:
-        return True
-    else:
-        print "Python: " + str(py_out)
-        print "Java: " + str(java_out)
-        return False
 
-check(*argt)
+
+    print "Python: " + str(py_out)
+    print "Java: " + str(java_out)
+
+
+for line in lines:
+    check(*argt)
