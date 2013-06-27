18acf4205b867a028622d31998831564df0a792a
Derrick Lin <drrckln@gmail.com>
2013-06-27 12:08:16 -0400

    moved java to quixey again so can import

---
+++ problems/quixey/KNAPSACK.java
@@ -0,0 +1,17 @@
+package quixey;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author derricklin
+ */
+public class KNAPSACK {
+    // no defaultdict..
+    // get returns null if no mapping for the key
+
+
+
+}
