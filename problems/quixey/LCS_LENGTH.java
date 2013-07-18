fc6a724c751c5fc30afdb87bed91cd4ec4f6060c
Derrick Lin <drrckln@gmail.com>
2013-07-18 03:59:03 -0700

    fixed lcs_length

--- problems/quixey/LCS_LENGTH.java
+++ problems/quixey/LCS_LENGTH.java
@@ -49,7 +49,7 @@ public class LCS_LENGTH {
         if (!dp.isEmpty()) {
             List<Integer> ret_list = new ArrayList<Integer>();
             for (int i=0; i<s.length(); i++) {
-                ret_list.add(Collections.max(dp.get(i).values()));
+                ret_list.add(!dp.get(i).isEmpty() ? Collections.max(dp.get(i).values()) : 0);
             }
             return Collections.max(ret_list);
         } else {
