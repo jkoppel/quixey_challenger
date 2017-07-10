8ffb32a9666c7e3abb2973f48d05b80dfb1e5476
Angela Chen <angchen@angchen-macbookpro.roam.corp.google.com>
2017-07-09 19:04:44 -0700

    Worked on Node-based algorithms for java programs

--- problems/java_programs/BREADTH_FIRST_SEARCH.java
+++ problems/java_programs/BREADTH_FIRST_SEARCH.java
@@ -1,5 +1,6 @@
 package java_programs;
 import java.util.*;
+import java.util.ArrayDeque;
 
 /*
  * To change this template, choose Tools | Templates
@@ -11,84 +12,30 @@ import java.util.*;
  * @author derricklin
  */
 public class BREADTH_FIRST_SEARCH {
-    public static boolean breadthfirstsearch(Node startnode, Node goalnode) {
-        ArrayDeque<Node> queue = new ArrayDeque<Node>();
+
+    public static Set<Node> nodesvisited = new HashSet<>();
+
+    public static boolean breadth_first_search(Node startnode, Node goalnode) {
+        Deque<Node> queue = new ArrayDeque<>();
         queue.addLast(startnode);
 
-        Set<Node> nodesseen = new HashSet<Node>();
-        nodesseen.add(startnode);
+        nodesvisited.add(startnode);
 
-        while (true) {
-            Node node = queue.getFirst();
-            queue.removeFirst();
+        while (!queue.isEmpty()) {
+            Node node = queue.removeFirst();
 
             if (node == goalnode) {
                 return true;
             } else {
-                for (Node cur_node : node.successors()) {
-                    if (!nodesseen.contains(cur_node)) {
-                        queue.addFirst(cur_node);
+                for (Node successor_node : node.getSuccessors()) {
+                    if (!nodesvisited.contains(successor_node)) {
+                        queue.addFirst(successor_node);
+                        nodesvisited.add(successor_node);
                     }
                 }
             }
+        }
         return false;
-        }
     }
 
-    public static class Node {
-        private List<Node> successors = new ArrayList<Node>();
-        public String name = "";
-
-        public List<Node> successors() {
-            return successors;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public boolean equals(Node other) {
-            if (successors == other.successors() && name.equals(other.name)) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        @Override
-        public String toString() {
-            return "Node: ".concat(name);
-        }
-
-        @Override
-        public int hashCode() {
-            String to_hash = name;
-            for (Node cur_node : successors) {
-                to_hash += cur_node.getName();
-            }
-            int hc = to_hash.hashCode();
-            return hc;
-
-        }
-        // need to write a hashCode function
-        // possibly toString()
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj == null) {
-                return false;
-            }
-            if (getClass() != obj.getClass()) {
-                return false;
-            }
-            final Node other = (Node) obj;
-            if (this.successors != other.successors && (this.successors == null || !this.successors.equals(other.successors))) {
-                return false;
-            }
-            if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name)) {
-                return false;
-            }
-            return true;
-        }
-    }
 }
---
+++ problems/java_programs/TOPOLOGICAL_ORDERING.java
@@ -0,0 +1,22 @@
+package java_programs;
+import java.util.*;
+
+class TOPOLOGICAL_ORDERING {
+    public static SortedSet<Node> topological_ordering (List<Node> directedGraph) {
+        SortedSet<Node> orderedNodes = new TreeSet<>();
+        for (Node node : directedGraph) {
+            if (node.getIncomingNodes().isEmpty()) {
+                orderedNodes.add(node);
+            }
+        }
+
+        for (Node node : orderedNodes) {
+            for (Node nextNodes : node.getSuccessors()) {
+                if (nextNodes.getSuccessors().containsAll(orderedNodes)) {
+                    orderedNodes.add(nextNodes);
+                }
+            }
+        }
+        return orderedNodes;
+    }
+}
