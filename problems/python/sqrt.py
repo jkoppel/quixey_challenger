1ae14d76580a45a0949669190b44f1422d1b1fb8
drrckln <drrckln@gmail.com>
2017-07-11 23:09:03 -0700

    removed extra sqrt variable

--- problems/python/sqrt.py
+++ problems/python/sqrt.py
@@ -3,7 +3,7 @@ def sqrt(x, epsilon):
     approx = x / 2
     while abs(x - approx) > epsilon:
         approx = 0.5 * (approx + x / approx)
-    return approx,sqrt
+    return approx
 
 """
 Square Root
