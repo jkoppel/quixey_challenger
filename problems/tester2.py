3ae3b56423106ff966874a3b69796b7d9b0afba8
drrckln <drrckln@gmail.com>
2017-07-12 01:45:49 -0700

    use deep copies, crap. discovered this trying to understand next_palindrome

--- problems/tester2.py
+++ problems/tester2.py
@@ -41,17 +41,17 @@ if __name__ == "__main__":
             # unsure how to make immutable; previous versions just used copy.deepcopy
 
         # check good Python version
-        py_out_good = py_try(algo+"GOOD", *test_in)
+        py_out_good = py_try(algo+"GOOD", *copy.deepcopy(test_in))
         print("Correct Python: " + prettyprint(py_out_good))
 
         # check bad Python version
-        py_out_test = py_try(algo, *test_in)
+        py_out_test = py_try(algo, *copy.deepcopy(test_in))
         print("Bad Python: " + prettyprint(py_out_test))
 
         # check bad Java version
         try:
             p1 = subprocess.Popen(["/usr/bin/java", "JavaDeserialization", algo]+ \
-                                [json.dumps(arg) for arg in test_in], stdout=subprocess.PIPE)
+                                [json.dumps(arg) for arg in copy.deepcopy(test_in)], stdout=subprocess.PIPE)
             java_out = p1.stdout.read()
             print("Bad Java:   " + prettyprint(java_out)[2:-3])
         except:
